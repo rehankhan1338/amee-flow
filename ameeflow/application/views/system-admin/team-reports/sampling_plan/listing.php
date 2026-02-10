00b82983e2da4edcc57c81aa0a08ee41f39dd7bb
rehankhan1338 <rehanaltaf888@gmail.com>
2026-02-11 01:13:31 +0300

    improved-design

--- ameeflow/application/views/system-admin/team-reports/sampling_plan/listing.php
+++ ameeflow/application/views/system-admin/team-reports/sampling_plan/listing.php
@@ -1,28 +1,30 @@
 <section class="content">
     <div class="box">         
        
-        <div class="box-header ">	
-            <div class="row">	
-                <div class="col-md-2">
-                    <label class="fw600">Year </label>
-                    <select class="form-control" id="yr" name="yr" onchange="return getSPTerms(this.value,'<?php if(isset($_GET['termId']) && $_GET['termId']!=''){echo $_GET['termId'];}else{echo '0';}?>');">
-                        <option value="0">All</option>
+        <div class="box-header no-border">	
+            <div class="row align-items-end">	
+                <div class="col-md-3">
+                    <label class="form-label fw600 mb-2">Year</label>
+                    <select class="form-control modern-select" id="yr" name="yr" onchange="return getSPTerms(this.value,'<?php if(isset($_GET['termId']) && $_GET['termId']!=''){echo $_GET['termId'];}else{echo '0';}?>');">
+                        <option value="0">All Years</option>
                         <?php foreach($spYearArr as $yr){?>
                         <option <?php if(isset($_GET['yr']) && $_GET['yr']==$yr['year']){?> selected <?php }?> value="<?php echo $yr['year'];?>"><?php echo $yr['year'];?></option>
                         <?php } ?>
                     </select>
                 </div>
-                <div class="col-md-2">
-                    <label class="fw600"><span id="termLoader">Term </span></label>
-                    <select class="form-control" id="termId" name="termId">
-                        <option value="0">All</option>
+                <div class="col-md-3">
+                    <label class="form-label fw600 mb-2"><span id="termLoader">Term</span></label>
+                    <select class="form-control modern-select" id="termId" name="termId">
+                        <option value="0">All Terms</option>
                         <?php if(count($termsOptions)>0){ foreach($termsOptions as $term){?>
                         <option <?php if(isset($_GET['termId']) && $_GET['termId']==$term['termId']){?> selected <?php }?> value="<?php echo $term['termId'];?>"><?php echo $this->config->item('terms_assessment_array_config')[$term['termId']]['name'];?></option>
                         <?php } } ?>
                     </select>
                 </div>
-                <div class="col-md-2">
-                        <button type="button" id="goBtn" onclick="return applySpFilter();" style="margin-top:20px;" class="btn btn-primary">Apply</button>
+                <div class="col-md-3">
+                    <button type="button" id="goBtn" onclick="return applySpFilter();" class="btn btn-primary btn-modern">
+                        <i class="fa fa-filter"></i> Apply Filter
+                    </button>
                 </div>
             </div>
         </div>
@@ -40,13 +42,35 @@
                         </tr>
                     </thead>
                     <tbody id="append_company_products">
-                        <?php $i = 1; foreach($samplingPlanDataArr as $row){ ?>
+                        <?php $i = 1; foreach($samplingPlanDataArr as $row){ 
+                            $hasFeedback = isset($row['feedbackCnt']) && $row['feedbackCnt'] > 0;
+                        ?>
                         <tr>
                             <td> <?php echo $i;?>  </td>
-                            <td class="fw600"> <a class="pro_name" onclick="return viewCompleteReport('<?php echo $row['spId'];?>','<?php echo $row['speId'];?>','<?php echo $row['userId'];?>','<?php echo $shareReportFor;?>');"> <?php echo $this->config->item('terms_assessment_array_config')[$row['termId']]['name'].' - '.$row['year'];?> <!-- &nbsp;<i id="vrpt<?php echo $row['spId'];?>" class="fa fa-info-circle" aria-hidden="true"></i>--> </a>  </td>
+                            <td class="fw600">
+                                <?php if($hasFeedback){ ?>
+                                    <span class="text-muted" style="cursor: not-allowed;" title="View disabled - Feedback available">
+                                        <?php echo $this->config->item('terms_assessment_array_config')[$row['termId']]['name'].' - '.$row['year'];?>
+                                        <i class="fa fa-lock text-muted ml-1" aria-hidden="true"></i>
+                                    </span>
+                                <?php } else { ?>
+                                    <a class="pro_name" onclick="return viewCompleteReport('<?php echo $row['spId'];?>','<?php echo $row['speId'];?>','<?php echo $row['userId'];?>','<?php echo $shareReportFor;?>');">
+                                        <?php echo $this->config->item('terms_assessment_array_config')[$row['termId']]['name'].' - '.$row['year'];?>
+                                    </a>
+                                <?php } ?>
+                            </td>
                             <td class="fw600" style="color:#3714D6;"> <?php echo $row['auName'];?> </td>
                             <td> <?php echo date('m/d/y, h:i A',$row['createTime']);?> </td>
-                            <td class="fw600"> <a id="vfbkLnk<?php echo $row['spId'];?>" class="pro_name" onclick="return viewFeedback('<?php echo $row['spId'];?>','<?php echo $shareReportFor;?>');">View</a> &nbsp;<?php echo '('.$row['feedbackCnt'].')';?> </td>
+                            <td class="fw600">
+                                <?php if($hasFeedback){ ?>
+                                    <a id="vfbkLnk<?php echo $row['spId'];?>" class="pro_name" onclick="return viewFeedback('<?php echo $row['spId'];?>','<?php echo $shareReportFor;?>');">
+                                        <span class="badge bg-warning text-dark">View Feedback</span>
+                                    </a>
+                                    <span class="text-muted"> (<?php echo $row['feedbackCnt'];?>)</span>
+                                <?php } else { ?>
+                                    <span class="text-muted">No feedback</span>
+                                <?php } ?>
+                            </td>
                             <td>
                                 <?php $spApprovalDataArr = getApprovalDataArrCh($row['spId'],$shareReportFor);
                                 if(count($spApprovalDataArr)>0){
@@ -84,14 +108,24 @@ function getSPTerms(year,selTerm){
 		$.ajax({
 			url: '<?php echo base_url().$this->config->item('system_directory_name');?>reports/ajaxGetSPTerms?uniAdminId='+uniAdminId+'&year='+year+'&selTerm='+selTerm,
 			beforeSend: function(){
-				$('#termLoader').html('<i class="fa fa-spinner fa-spin"></i> Please Wait');
-				$('#termId').html('<option value="">Please Wait...</option>');
+				$('#termLoader').html('<i class="fa fa-spinner fa-spin"></i> Loading...');
+				$('#termId').html('<option value="0">Loading...</option>');
+				$('#termId').prop('disabled', true);
 			},
 			success: function(result, status, xhr){
 				$('#termId').html(result);
-				$('#termLoader').html('Term *');
+				$('#termId').prop('disabled', false);
+				$('#termLoader').html('Term');
+			},
+			error: function(){
+				$('#termId').html('<option value="0">All Terms</option>');
+				$('#termId').prop('disabled', false);
+				$('#termLoader').html('Term');
 			}
 		});
+	} else {
+		$('#termId').html('<option value="0">All Terms</option>');
+		$('#termId').prop('disabled', false);
 	}
 }
 </script>
